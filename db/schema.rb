e90a0d3ddec5cb6d48ba9d776ed72067860536cb
Seth Lieberman <shefseth@gmail.com>
2016-02-19 16:21:59 -0500

    updated post table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218201916) do
+ActiveRecord::Schema.define(version: 20160219212120) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "username"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160218201916) do
     t.string   "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "group_id"
   end
 
   create_table "user_groups", force: :cascade do |t|
