abdbe5144629c27a531173c5daa4614f92f34b58
Seth Lieberman <shefseth@gmail.com>
2016-02-19 16:54:16 -0500

    comments work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,13 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @group = Group.find(params[:group_id])
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
-    redirect_to user_path(@current_user)
+    redirect_to user_group_path(@user.id, @group.id)
   end
 
   def edit
@@ -26,7 +28,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    comment_params = params.require(:comment).permit(:content)
-    comment_params
+     params.require(:comment).permit(:content)
+    
   end
 end
