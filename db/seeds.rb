5ebfec333c670b46396b80d1f11722df79e7e047
Seth Lieberman <shefseth@gmail.com>
2016-02-19 22:38:08 -0500

    link updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create([
+	{username:"ShefSeth", password:"pw", fname:"Seth", lname:"Lieberman", email:"shefseth@gmail.com",zipcode: "19103"},
+	{username:"Bob The Slob", password:"pw", fname:"Bob", lname:"Pooperton", email:"b@b",zipcode: "19104"},
+	{username:"Bess The Mess", password:"pw", fname:"Bess", lname:"Hessper", email:"bess@b",zipcode: "08003"},
+	{username:"Jilly From Philly", password:"pw", fname:"Jillian", lname:"Lieberman", email:"j@j",zipcode: "19103"},
+	{username:"Billy Bob", password:"pw", fname:"Billy", lname:"Thorton", email:"billy@b",zipcode: "19107"},
+	{username:"Stan the Man", password:"pw", fname:"Stan", lname:"Smith", email:"s@s",zipcode: "19107"}
+	])
+
+Group.create([
+	{title:"Lieberman's Group"},
+	{title:"Friends Forever"},
+	{title:"HighSchool"},
+	{title:"Party Group"},
+	{title:"Lieberman's Group"},
+	{title:"Lieberman's Group"},
+	{title:"Lieberman's Group"}
+	])
