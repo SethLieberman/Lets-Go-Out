5675216d5b816f4fa4c9af82c02cae533541c85c
Seth Lieberman <shefseth@gmail.com>
2016-02-25 15:33:30 -0500

    fixed gmaps errors

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,10 +25,10 @@ class PlacesController < ApplicationController
 
 
 	def index
-		@hash = Gmaps4rails.build_markers(@users) do |user, marker|
-			marker.lat user.latitude
-			marker.lng user.longitude
-		end
+		# @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+		# 	marker.lat user.latitude
+		# 	marker.lng user.longitude
+		# end
 
 		puts "HASHING is #{@hash}"
 
